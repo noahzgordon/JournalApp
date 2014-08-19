89bd3244b71049560721ee468643dd134dfa4e30
Zak and Noah <zak+noah@appacademy.io>
2014-08-19 10:56:59 -0400

    Added delete button and listeners for various events to Posts Index view.

--- app/assets/javascripts/views/posts/posts_index.js
+++ app/assets/javascripts/views/posts/posts_index.js
@@ -1,6 +1,25 @@
 JournalApp.Views.PostsIndex = Backbone.View.extend({
   template: JST['posts/index'],
 
+  initialize: function() {
+    this.listenTo(
+      JournalApp.Collections.posts,
+      "add change:title remove reset",
+      this.render
+    );
+  },
+
+  events: {
+    "click .post-delete": "deletePost"
+  },
+
+  deletePost: function(event) {
+    var id = $(event.target).attr("data-id")
+    var post = JournalApp.Collections.posts.get(id)
+
+    post.destroy();
+  },
+
   render: function () {
     this.$el.html(this.template({
       posts: JournalApp.Collections.posts.models
